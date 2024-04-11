0d847323d25216fb4d5f92c258519938bc8f9ad5
Kiran-Ee <earnsha2@uwm.edu>
2024-04-11 18:03:44 -0500

    4/11/24: KE - Wrote PadBinaryTest - ^

--- src/MachineCode/GeneralMachineCode.java
+++ src/MachineCode/GeneralMachineCode.java
@@ -20,4 +20,11 @@ public class GeneralMachineCode {
     public String hex_to_binary(String hex) {
         return null;
     }
+
+    public String pad_binary(String binary_instr, int num_padding) {
+        for (int i = 0; i < num_padding; i++) {
+            binary_instr = "0" + binary_instr;
+        }
+        return binary_instr;
+    }
 }
