d07f3808c8ea97cb3304080f8a26868a36e18677
Kiran-Ee <earnsha2@uwm.edu>
2024-04-13 17:19:43 -0500

    4/13/24: KE - PASSING BeqTest - ^

--- Tests/Operations/BeqTest.java
+++ Tests/Operations/BeqTest.java
@@ -8,20 +8,11 @@ import static org.junit.Assert.assertEquals;
 //Machine-BEQ, rs, rt, offset
 public class BeqTest {
     String bin_instr = "00010010001100010000000000100000"; //"beq", "$s0", "$s1", "32" -hex-"12310020
-    //bin 00010010001100010000000000100000
-    String exMnem = "beq {opcode: 000100, rs: 10000, rt: 10001, offset: 0000000000100000}";
+    String exMnem = "beq {opcode: 04, rs(base): 11, rt: 11, immediate(offset): 0020}";
     String bin_instr1 = "00010001101000100000000000000000"; //beq t5 v0 0x0000 hex-0x11A20000
-    String exMnem1 = "beq {opcode: 000100, rs: 01101, rt: 00010, offset: 1000000000000000}";
+    String exMnem1 = "beq {opcode: 04, rs(base): 0d, rt: 02, immediate(offset): 0000}";
     String bin_instr2 = "00010010000001110000000100000000"; //beq $s0 $a3 "4" hex-12070100
-    String exMnem2 = "beq {opcode: 000100, rs: 10000, rt: 01110, offset: 0000000100000000";
-    //    String[] valid_instr = {"beq", "$s0", "$s1", "32"}; //Format: beq, rs, rt, offset
-//    String[] valid_instr1 = {"beq", "$t0", "$zero", "4"};
-//    String[] valid_instr2 = {"beq", "$20", "$13", "10"}; //"beq", "$s4", "$t5", "10"
-//    String[] zero_offset = {"000100", "$s1", "$s2", "000000"};
-//    String[] negative_offset = {"beq", "$s1", "$s3", "-200"}; //-proj instruction said will test positive val only
-//
-//    String[] prof1 = {"beq", "$s0", "$a0", "0x34"}; // MC: 000100 10000 00100 0000000000110100
-//
+    String exMnem2 = "beq {opcode: 04, rs(base): 10, rt: 07, immediate(offset): 0100}";
     @Test
     public void good_variable_settings1() {
         Beq beq = new Beq(bin_instr);
