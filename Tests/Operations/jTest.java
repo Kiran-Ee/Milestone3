c5af3e7f0c20423bd61529c01e59f8ba4a137ce2
Kiran-Ee <earnsha2@uwm.edu>
2024-04-13 17:27:22 -0500

    4/13/24: KE - PASSING jTest - ^

--- Tests/Operations/jTest.java
+++ Tests/Operations/jTest.java
@@ -7,11 +7,9 @@ import static org.junit.Assert.assertEquals;
 //Format: J target
 //Machine: J, instr_index(26)
 public class jTest {
-    //    String[] instr = {"j", "2500"}; //format: j, target
-//    String[] instr_hex = {"j", "0x1F4"};
 
     String bin_instr = "00001000000000000000000111110100"; //"j", "0x1F4, hex-080001F4
-    String exMnem = "j {opcode: 000010, address: 00000000000000000001000000010100";
+    String exMnem = "j {opcode: 02, index: 00001f4}";
 
     @Test
     public void good_variable_setting1(){
