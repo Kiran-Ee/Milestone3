957064f3dbe6cf4d9028db6d7fd980e4e561e986
Kiran-Ee <earnsha2@uwm.edu>
2024-04-14 16:47:25 -0500

    4/14/24: KE - Jar Update - ^

--- src/Main.java
+++ src/Main.java
@@ -10,35 +10,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "src/Profs_Test_File";
-        int totalPassed = 0;
-        int debugcounter = 0; // for conditional breakpoints ...
-
-        try (FileReader fileReader = new FileReader(filePath); BufferedReader bufferReader = new BufferedReader(fileReader)) {
-            String line;
-            while ((line = bufferReader.readLine()) != null) {
-                debugcounter++;
-                String[] parts = line.split(" ", 2); //find at most 2 parts of the string seperated by space
-
-                String hex_instr = parts[0];
-                String exp_mnenomic = parts[1];
-
-                GeneralMachineCode gmc = new GeneralMachineCode();
-                String actual_mnenomic = gmc.hex_to_mnenomic(hex_instr);
-
-                System.out.println("Counter " + debugcounter);
-                if (actual_mnenomic.equals(exp_mnenomic)) {
-                    System.out.println("+++ Verification passed for: " + line);
-                    totalPassed++;
-                } else {
-                    System.out.println("--- Verification failed for: " + line);
-                    System.out.println("   Expected: " + exp_mnenomic);
-                    System.out.println("   Actual: " + actual_mnenomic);
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Error opening file: " + filePath, e);
-        }
-        System.out.println("Total Passed: " + totalPassed + " out of 1000");
+        GeneralMachineCode gmc = new GeneralMachineCode();
+        String hex_instr = args[0];
+        String mnenomic = gmc.hex_to_mnenomic(hex_instr);
+        System.out.println(mnenomic);
     }
 }
